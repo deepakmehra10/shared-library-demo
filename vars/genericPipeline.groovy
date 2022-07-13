d76053dea1d3fcb14176203e281aed818e909fba
mac <mac@MANMOHANs-Air.Dlink>
2022-07-13 17:28:36 +0530

    hello

--- vars/genericPipeline.groovy
+++ vars/genericPipeline.groovy
@@ -1,28 +1,3 @@
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('build') {
-                when{
-                    branch 'master'
-                }
-                steps {
-                    script {
-                        withCredentials([
-                                usernamePassword(credentialsId: 'apigee-credentials',
-                                        usernameVariable: 'username',
-                                        passwordVariable: 'password')
-                        ]) {
-                            print 'username=' + username + 'password=' + password
-                            print 'username.collect { it }=' + username.collect { it }
-                            print 'password.collect { it }=' + password.collect { it }
-                            environment = "test"
-                            sh "echo 'Hello testing...'"
-                            sh "curl -XGET 127.0.0.1:9000/genericPipeline"
-                        }
-                    }
-                }
-            }
-        }
-    }
+    echo "Hi I was called"
 }
